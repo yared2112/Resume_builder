04b4fc8edc1391bc1a12a9ab6e4dc6aca01fbfc4
yared2112 <teklewold.yared21@gmail.com>
2022-06-17 02:44:19 +0300

    completed task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { UserForm } from './components/UserForm';
 
 const App = () => {
   return (
     <div className="App">
-      RESUME BUILDER
+      <UserForm />
     </div>
   );
 }
